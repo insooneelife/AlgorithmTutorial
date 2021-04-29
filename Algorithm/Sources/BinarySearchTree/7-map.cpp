e27cb31b055593f6828003b644e8da18c478a6c4
unknown <insooneelife@naver.com>
2021-04-29 20:00:19 +0900

    fixed

---
+++ Algorithm/Sources/BinarySearchTree/7-map.cpp
@@ -0,0 +1,51 @@
+﻿// STL Binary Search Tree
+// C++ STL에는 이미 잘 만들어진 이진탐색트리가 존재한다. (레드블랙트리)
+
+#include <iostream>
+#include <map>
+#include "Print.h"
+using namespace std;
+
+int main()
+{
+    // 키 값을 기준으로 원소가 정렬됨
+    map<int, string> tree = { {1, "blbl"}, {5, "ffsf"}, {3, "kgkgk"} , {13, "bba"} , {7, "dddd"} , {8, "eeee"} };
+
+    Print::Map(tree, "default");
+
+    // 탐색
+    auto it = tree.find(5);
+    if (it != end(tree))
+    {
+        cout << "search : " << it->first << " " << it->second << endl << endl;
+    }
+
+    // 삽입
+    tree.insert(make_pair(8, "ffds"));
+    tree[-55] = "kgkg";
+
+    Print::Map(tree, "insert");
+
+    // 삭제
+    tree.erase(1);
+    Print::Map(tree, "delete");
+    
+
+    // 이진탐색트리는 내부적으로 정렬된 상태로 데이터를 관리하기 때문에,
+    // 범위 기반 질의(Range Based Query)에 강하다.
+    // STL map에서도 범위기반 질의를 편하게 사용하도록 하기 위해, lower_bound와 upper_bound를 지원한다.
+    // 이 연산자는 find와 달리 특정 원소를 타겟으로 반복자 위치를 찾는것이 아니라,
+    // 경계값으로 키 값을 넣어서 트리 내부에서 그 값의 경계가 되는 반복자 위치를 가져오도록 한다. (경계는 위를 기준으로 or 아래를 기준으로 가능하다.)
+    // upper_bound와 lower_bound로 두 개의 경계가 되는 반복자 위치를 가져왔다면,
+    // 이 위치를 기반으로 for 루프를 돌며 그 범위 내의 모든 값을 순회할 수 있다.
+
+    auto lower = tree.lower_bound(3);
+    auto upper = tree.upper_bound(7);
+
+    for (auto it = lower; it != upper; ++it)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+
+    return 0;
+}
---
+++ Algorithm/Sources/BinarySearchTree/8-map-CustomKey.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include "Print.h"
+using namespace std;
+
+// 위치를 키로 하고 이름 값으로 저장하는 map을 생각해보자.
+// 하지만 tree에서 데이터에 대해 탐색, 삽입, 삭제 등 모든 연산을 수행하기 위해서 키에 대한 비교가 가능해야한다.
+// 그러므로 Point 구조체에는 비교 연산자를 정의해줄 필요가 있다.
+
+
+struct Point
+{
+    Point() :x(0), y(0) {}
+    Point(int x, int y) : x(x), y(y) {}
+
+
+    // for less
+    bool operator<(const Point& other) const
+    {
+        if (x == other.x)
+            return y < other.y;
+
+        return x < other.x;
+    }
+
+    // for greater
+    bool operator>(const Point& other) const
+    {
+        if (x == other.x)
+            return y > other.y;
+
+        return x > other.x;
+    }
+
+
+public:
+    int x;
+    int y;
+};
+
+int main()
+{
+    map<Point, string> tree = { {Point(5, 7), "june"}, {Point(5, 7), "tom"}, {Point(5, 7), "jack"}, {Point(5, 7), "mina"} };
+
+    for (auto e : tree)
+    {
+        cout << e.first.x << " " << e.first.y << " " << e.second << endl;
+    }
+    cout << endl;
+
+
+    // 같은 방법으로 set도 이용 가능하다.
+    // set은 unordered_set과 같은 인터페이스를 이용하며,
+    // 값은 따로 저장하지 않고 키만 저장하는 형태의 이진탐색트리이다.
+
+    set<Point> tree2 = { Point(5, 7), Point(5, 8), Point(6, 6), Point(6, 7),Point(6, 8), };
+
+    for (Point p : tree2)
+    {
+        cout << p.x << " " << p.y << endl;
+    }
+    cout << endl;
+
+
+    // 또한 map, set 모두 키에 대해 오름차순으로 트리를 유지할 수도 있겠지만,
+    // 내림차순으로 트리를 유지해야하는 상황도 생길 수 있을 것이다.
+    // 그런 상황을 위해 map과 set은 트리 내부 비교 연산 정책을 수정할 수 있도록 설계되어있다.
+    // 하지만 내림차순으로 트리를 구성하려면 operator>를 추가로 정의해주어야 한다.
+    set<Point, greater<Point>> tree3 = { Point(5, 7), Point(5, 8), Point(6, 6), Point(6, 7),Point(6, 8), };
+
+    for (Point p : tree3)
+    {
+        cout << p.x << " " << p.y << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
