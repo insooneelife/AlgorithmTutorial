59c5cd205718bb34f60222505b14ef35d78529a4
hanbin8269 <hanbin8269@gmail.com>
2021-05-01 14:36:02 +0900

    unordered map char count

--- Algorithm/Sources/Student Playgrounds/hanbin8269/Playground.cpp
+++ Algorithm/Sources/Student Playgrounds/hanbin8269/Playground.cpp
@@ -1,10 +1,16 @@
 ﻿// hanbin8269 의 작업공간
 #include <iostream>
-
+#include <unordered_map>
 using namespace std;
 
 int main()
 {
-    cout << "hello world!" << " "
-    return 0;
+    string text = "aaabbbccc";
+    unordered_map<char, int> um;
+    for (int i = 0; i < text.size(); i++) {
+        um[text.at(i)] += 1;
+    }
+    for (auto elem : um) {
+        cout << "key : " << elem.first << " value : " << elem.second << endl;
+    }
 }
