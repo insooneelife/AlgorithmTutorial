5f7e09ca8bed706b3e647c6954f2dd5dd3b1568c
hmoon630 <s19017@gsm.hs.kr>
2021-05-01 14:26:22 +0900

    count char

--- Algorithm/Sources/Student Playgrounds/hmoon630/Playground.cpp
+++ Algorithm/Sources/Student Playgrounds/hmoon630/Playground.cpp
@@ -1,10 +1,25 @@
 ﻿// hmoon630 의 작업공간
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
+    string text = "aabbbbccccdkkdddd";
+
+    unordered_map<char, int> counts;
+
+    for (auto c : text)
+    {
+        counts[c]++;
+    }
+
+    for (auto e : counts)
+    {
+        cout << e.first << " " << e.second << endl;
+    }
+
     return 0;
 }
