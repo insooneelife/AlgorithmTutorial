76952fd85e8ea0d5660a126b97544d7ddf9f8529
unknown <insooneelife@naver.com>
2021-05-07 20:27:25 +0900

    Union Find

---
+++ Algorithm/Sources/UnionFind/1-Union-Find.cpp
@@ -0,0 +1,76 @@
+﻿// 상호 배타적 집합
+// 단순한 구현
+// 각 원소의 부모 원소가 저장될 parent 배열을 정의한다.
+
+// Init 
+// 시작 시, 모든 원소는 각각이 유일한 집합으로 표현된다. 
+// 그러므로 각 원소들을 모두 루트이고, parent로 자기자신을 참조한다.
+
+// Find
+// 어떤 원소가 속한 집합을 알기 위해, 해당 원소의 루트 번호를 찾는다.
+// parent 배열을 거슬러 올라가면 루트 번호에 도달하게 된다.
+
+// Union
+// 어떤 두 원소를 합치기 위해,
+// 두 원소가 속한 두 집합의 루트 번호를 찾고,
+// 두 집합이 서로 다른 집합이라면 (루트 번호가 다르다면),
+// 한쪽 루트가 다른쪽 루트를 부모로 참조한다. 
+// 그러면 집합이 합쳐진다.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct UnionFind
+{
+    vector<int> parent;
+
+    UnionFind(int n) : parent(n)
+    {
+        // 모든 원소들은 자기자신을 부모로 갖는다.
+        // 즉, 각각이 하나의 유일한 집합의 루트가 된다.
+        for (int i = 0; i < n; ++i)
+        {
+            parent[i] = i;
+        }
+    }
+
+    // i가 속한 집합의 루트 번호
+    int Find(int i) const
+    {
+        if (i == parent[i]) return i;
+        return Find(parent[i]);
+    }
+
+    // 두 집합 a, b를 합친다.
+    void Union(int a, int b)
+    {
+        int root1 = Find(a);
+        int root2 = Find(b);
+
+        if (root1 == root2)
+            return;
+
+        parent[root1] = root2;
+    }
+};
+
+
+int main()
+{
+    const int N = 10;
+    UnionFind uf(N);
+
+    uf.Union(1, 3);
+    uf.Union(3, 5);
+    uf.Union(6, 7);
+    uf.Union(6, 9);
+    uf.Union(1, 6);
+
+    for (int i = 0; i < N; ++i)
+    {
+        cout << i << " " << uf.Find(i) << endl;
+    }
+
+    return 0;
+}
---
+++ Algorithm/Sources/UnionFind/2-Union-Find-Rank.cpp
@@ -0,0 +1,75 @@
+﻿// 상호 배타적 집합
+// 랭크를 이용한 Union 최적화 구현
+
+// 트리를 이용하여 자료구조를 설계하면 O(logN)의 성능으로 Find와 Union이 둘 다 가능하다.
+// 하지만 이 성능은 트리가 균형잡혀 있을경우에만 해당된다.
+// 만약 트리가 한쪽으로 완전하게 치우쳐져 있다면, 배열과 크게 다를것이 없을 것이다.
+// 이 문제를 피하기 위해, 
+// 트리의 높이를 비교하여 낮은쪽 트리를 높은쪽 트리에 붙이는 전략을 사용할 수 있다.
+// 이러한 전략을 랭크에 의한 합치기(Union by Rank)라고 부른다.
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct RankedUnionFind
+{
+    vector<int> parent;
+    vector<int> rank;
+
+    // 모든 트리의 높이 (rank)는 1로 초기화
+    RankedUnionFind(int n) : parent(n), rank(n, 1)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            parent[i] = i;
+        }
+    }
+
+    // i가 속한 집합의 루트 번호
+    int Find(int i)
+    {
+        if (i == parent[i]) return i;
+        
+        // 경로 압축을 이용한 최적화
+        return parent[i] = Find(parent[i]);
+    }
+
+    // 두 집합 a, b를 합친다.
+    void Union(int a, int b)
+    {
+        int root1 = Find(a);
+        int root2 = Find(b);
+
+        if (root1 == root2)
+            return;
+
+        if (rank[root1] > rank[root2]) swap(root1, root2);
+        // 이제 rank[root2]가 rank[root1]보다 크거나 같으므로 root1을 root2의 자식으로 붙인다.
+        parent[root1] = root2;
+
+        // 트리의 높이가 같았었다면, 합친 후의 높이는 1 증가한다.
+        if (rank[root1] == rank[root2]) ++rank[root2];
+    }
+};
+
+
+int main()
+{
+    const int N = 10;
+    RankedUnionFind uf(N);
+
+    uf.Union(1, 3);
+    uf.Union(3, 5);
+    uf.Union(6, 7);
+    uf.Union(6, 9);
+    uf.Union(1, 6);
+
+    for (int i = 0; i < N; ++i)
+    {
+        cout << i << " " << uf.Find(i) << endl;
+    }
+
+    return 0;
+}
