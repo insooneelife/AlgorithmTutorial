1cf5b6b401c2af2d79086953b37a90d86c4b2936
unknown <insooneelife@naver.com>
2021-05-20 23:22:16 +0900

    problem solved

---
+++ Algorithm/Sources/Bitmask/Solutions/Programmers/점프와 순간 이동.cpp
@@ -0,0 +1,77 @@
+﻿// 점프와 순간 이동 
+// https://programmers.co.kr/learn/courses/30/lessons/12980
+
+// 시행착오
+// 1. BFS 도전 (시간초과)
+
+#include <bitset>
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    bitset<64> b(n);
+    return b.count();
+}
+
+int main()
+{
+    int n = 5000;
+    cout << solution(n);
+
+    return 0;
+}
+
+/*
+static int BFS(int finish)
+{
+    using namespace std;
+    using Node = pair<int, int>;
+
+    set<int> visited;
+
+    queue<Node> que;
+    int ret = -1;
+    const int start = 0;
+
+    que.push({ start, 0 });
+    visited.insert(start);
+
+    while (!que.empty())
+    {
+        Node state = que.front();
+        int node = state.first;
+        int cnt = state.second;
+
+        if (node == finish)
+        {
+            ret = cnt;
+            break;
+        }
+
+        vector<int> nexts = { node * 2, node + 1 };
+        vector<int> counts = { cnt, cnt + 1 };
+
+        for (int i = 0; i < nexts.size(); ++i)
+        {
+            int next = nexts[i];
+            int nextcnt = counts[i];
+
+            if (visited.find(next) == end(visited))
+            {
+                que.push({ next, nextcnt });
+                visited.insert(next);
+            }
+        }
+
+        que.pop();
+    }
+
+    return ret;
+}
+
+int solution(int n)
+{
+    return BFS(n);
+}
+*/
---
+++ Algorithm/Sources/UnionFind/Solutions/Programmers/섬 연결하기.cpp
@@ -0,0 +1,80 @@
+﻿
+// 섬 연결하기 (크루스칼)
+// https://programmers.co.kr/learn/courses/30/lessons/42861
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+//#include "Print.h"
+using namespace std;
+
+struct RankedUnionFind
+{
+    vector<int> parent;
+    vector<int> rank;
+
+    RankedUnionFind(int n) : parent(n), rank(n, 1)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int Find(int i)
+    {
+        if (i == parent[i]) return i;
+        return parent[i] = Find(parent[i]);
+    }
+
+    void Union(int a, int b)
+    {
+        int root1 = Find(a);
+        int root2 = Find(b);
+
+        if (root1 == root2)
+            return;
+
+        if (rank[root1] > rank[root2]) swap(root1, root2);
+        parent[root1] = root2;
+
+        if (rank[root1] == rank[root2]) ++rank[root2];
+    }
+};
+
+int solution(int n, vector<vector<int>> costs) 
+{
+    RankedUnionFind uf(n);
+
+    sort(begin(costs), end(costs), [](const vector<int>& a, const vector<int>& b) { return a[2] < b[2]; });
+    int answer = 0;
+
+    for (int i = 0; i < costs.size(); ++i)
+    {
+        int u = costs[i][0];
+        int v = costs[i][1];
+        int cost = costs[i][2];
+
+        int ru = uf.Find(u);
+        int rv = uf.Find(v);
+
+        if (ru != rv)
+        {
+            uf.Union(ru, rv);
+            answer += cost;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int n = 4;
+    vector<vector<int>> costs = { {0, 1, 1} ,{0, 2, 2},{1, 2, 5},{1, 3, 1},{2, 3, 8} };
+
+    cout << solution(n, costs);
+
+    return 0;
+}
