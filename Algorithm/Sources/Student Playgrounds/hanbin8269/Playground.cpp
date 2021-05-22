725947f4ea0079532b9dc7610bae6f5b854a0d55
hanbin8269 <58972963+hanbin8269@users.noreply.github.com>
2021-05-22 15:17:11 +0900

    블로그에 정리한 내용 추가

--- Algorithm/Sources/Student Playgrounds/hanbin8269/Playground.cpp
+++ Algorithm/Sources/Student Playgrounds/hanbin8269/Playground.cpp
@@ -1,16 +1,32 @@
-﻿// hanbin8269 의 작업공간
-#include <iostream>
-#include <unordered_map>
-using namespace std;
+// hanbin8269 의 작업공간
 
-int main()
-{
-    string text = "aaabbbccc";
-    unordered_map<char, int> um;
-    for (int i = 0; i < text.size(); i++) {
-        um[text.at(i)] += 1;
-    }
-    for (auto elem : um) {
-        cout << "key : " << elem.first << " value : " << elem.second << endl;
-    }
-}
+// Stack, Queue
+// 프로그래머스 기능개발
+// https://hanbin8269.tistory.com/21
+// 프로그래머스 다리를 지나는 트럭
+// https://hanbin8269.tistory.com/23
+// 프로그래머스 프린터
+// https://hanbin8269.tistory.com/25
+// 프로그래머스 크레인 인형뽑기
+// https://hanbin8269.tistory.com/33
+
+// Heap
+// 프로그래머스 더 맵게
+// https://hanbin8269.tistory.com/28
+
+// DFS
+// 프로그래머스 타겟넘버
+// https://hanbin8269.tistory.com/30
+
+// Hash
+// 프로그래머스 위장
+// https://hanbin8269.tistory.com/18
+// 프로그래머스 베스트 앨범
+// https://hanbin8269.tistory.com/19
+// 프로그래머스 전화번호 목록
+// https://hanbin8269.tistory.com/26
+
+
+// Greedy
+// 프로그래머스 체육복
+// https://hanbin8269.tistory.com/20
