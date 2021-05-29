15cff1420fd9f650a36f5176acff3970abff2e9a
unknown <insooneelife@naver.com>
2021-05-30 00:37:29 +0900

    problem solved

---
+++ Algorithm/Sources/Greedy/Solutions/Programmers/구명보트.cpp
@@ -0,0 +1,53 @@
+﻿
+// 구명보트 (탐욕법, 힙)
+// https://programmers.co.kr/learn/courses/30/lessons/42885
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) 
+{
+    priority_queue<int, vector<int>, greater<>> small;
+    priority_queue<int, vector<int>, less<>> big;
+    sort(begin(people), end(people));
+    int half = limit / 2;
+
+    for (int i = 0; i < people.size(); ++i)
+    {
+        if (people[i] <= half)
+            small.push(people[i]);
+        else
+            big.push(people[i]);
+    }
+
+    int cnt = 0;
+    while (!small.empty() && !big.empty())
+    {
+        if (small.top() + big.top() <= limit)        
+            small.pop();        
+
+        cnt++;
+        big.pop();
+    }
+
+    return (small.size() + 1) / 2 + big.size() + cnt;
+}
+
+int main()
+{
+    vector<int> people = { 70, 50, 80, 50 };
+    int limit = 100;
+
+    //vector<int> people = { 70, 80, 50};
+    //int limit = 100;
+
+    int ans = solution(people, limit);
+    cout << ans << endl;
+
+    return 0;
+}
---
+++ Algorithm/Sources/Greedy/Solutions/Programmers/큰 수 만들기.cpp
@@ -0,0 +1,99 @@
+﻿
+// 큰 수 만들기 (탐욕법)
+// https://programmers.co.kr/learn/courses/30/lessons/42883
+
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+#include <string>
+using namespace std;
+
+string solution(string number, int k)
+{
+    vector<pair<int, vector<int>>> num_idx(10);
+    string answer = "";
+
+    k = number.size() - k;
+
+    for (int i = 0; i < number.size(); ++i)
+    {
+        int num = number[i] - '0';
+        num_idx[num].second.push_back(number.size() - i);
+    }
+    
+    int last = 0;
+    while (k > 0)
+    {
+        bool found = false;
+        for (int i = num_idx.size() - 1; i >= 0; --i)
+        {
+            int val = i;
+            int& idx = num_idx[i].first;
+            vector<int>& que = num_idx[i].second;
+
+            if (idx < que.size())
+            {
+                int back = que[idx];
+                if (found)
+                {
+                    while (idx < que.size() && que[idx] > last)
+                    {
+                        idx++;
+                    }
+                }
+                else if(back >= k)
+                {
+                    answer += (val + '0');
+                    last = back;
+                    idx++;
+                    k--;
+                    found = true;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    string number = "4177252841";
+    int k = 4;
+
+    string ans = solution(number, k);
+    cout << ans << endl;
+
+    return 0;
+}
+
+
+
+#include <fstream>
+#include <iostream>
+void MakeTestcase()
+{
+    string number = "";
+    for (int i = 0; i < 1000000; ++i)
+    {
+        number += '1';
+    }
+    int k = number.size() / 2;
+
+    string ans = solution(number, k);
+    cout << number.size() << " " << k << " " << ans.size() << endl;
+
+    // make testcase;
+    string filePath = "test.txt";
+
+    // write File
+    ofstream writeFile(filePath.data());
+    if (writeFile.is_open())
+    {
+        writeFile << number << endl;
+        writeFile << ans << endl;
+        writeFile.close();
+    }
+}
