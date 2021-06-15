e4acba0101e2eb21552e7c99e8db411e6537200c
unknown <insooneelife@naver.com>
2021-06-16 01:41:11 +0900

    problems solved

---
+++ Algorithm/Sources/DP/Solutions/Programmers/단어 퍼즐.cpp
@@ -0,0 +1,84 @@
+﻿// 단어 퍼즐 (DP)
+// https://programmers.co.kr/learn/courses/30/lessons/12983
+
+// 시행착오
+// 1. t 문자열의 길이 범위를 잘못 확인함
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int MAX = numeric_limits<int>::max();
+ll mem[20001] = { 0 };
+
+bool MatchBackward(const string& str, int n, const string& sub)
+{
+    int size = sub.size();
+    for (int j = 0; j < size; ++j)
+    {
+        if (sub[j] != str[n - size + j])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll F(const vector<string>& strs, string& t, int n)
+{
+    if (n < 0)
+        return MAX;
+
+    if (n == 0)
+        return 0;
+
+    if (mem[n] > 0)
+        return mem[n];
+
+    ll minv = MAX;
+
+    for (int i = 0; i < strs.size(); ++i)
+    {
+        int size = strs[i].size();
+        if (n - size < 0)
+            continue;
+
+        if (MatchBackward(t, n, strs[i]))
+        {
+            minv = min(minv, F(strs, t, n - size) + 1LL);
+        }
+    }
+
+    return mem[n] = minv;
+}
+
+int solution(vector<string> strs, string t)
+{
+    for (int i = 0; i < 20001; ++i)
+        mem[i] = 0;
+
+    ll val = F(strs, t, t.size());
+    return val >= MAX ? -1 : val;
+}
+
+
+
+int main()
+{
+    //vector<string> strs = {"ba", "na", "n", "a"};
+    //string t = "banana";
+
+    //vector<string> strs = { "app","ap","p","l","e","ple","pp" };
+    //string t = "apple";
+
+    vector<string> strs = { "ba","an","nan","ban","n" };
+    string t = "banana";
+
+    cout << solution(strs, t);
+
+    return 0;
+}
+
+
---
+++ Algorithm/Sources/Greedy/Solutions/Programmers/디스크 컨트롤러.cpp
@@ -0,0 +1,78 @@
+﻿
+// 디스크 컨트롤러 (힙)
+// https://programmers.co.kr/learn/courses/30/lessons/42627
+
+// 시행착오
+// 1. 하드디스크가 작업을 수행하고 있지 않을 때에는 먼저 요청이 들어온 작업부터 처리합니다.
+// 위 조건으로 인해 문제 설계가 굉장히 많이 변하게 된다.
+
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+
+struct Data
+{
+    int start;
+    int length;
+
+    bool operator<(const Data& other) const
+    {
+        return length < other.length;
+    }
+
+    bool operator>(const Data& other) const
+    {
+        return length > other.length;
+    }
+};
+
+int solution(vector<vector<int>> jobs) 
+{
+    priority_queue<Data, vector<Data>, greater<Data>> pq;
+
+    sort(begin(jobs), end(jobs));
+
+    int i = 0;
+    int time = 0;
+    int sum = 0;
+    while (i < jobs.size() || !pq.empty()) 
+    {
+        if (jobs.size() > i && time >= jobs[i][0]) 
+        {
+            pq.push({ jobs[i][0], jobs[i][1] });
+            i++;
+            continue;
+        }
+        if (!pq.empty()) 
+        {
+            Data top = pq.top();
+            time += top.length;
+            sum += time - top.start;
+            pq.pop();
+        }
+        else
+        {
+            time = jobs[i][0];
+        }
+    }
+
+    return sum / jobs.size();
+}
+
+int main()
+{
+    vector<vector<int>> jobs = { {0, 3}, {1, 9}, {2, 6} };
+    //vector<vector<int>> jobs = { {0, 3}, {2, 7}, {6, 2} }; // 17
+    //vector<vector<int>> jobs = { {0, 3}, {4, 2}, {2, 7} }; // 16
+    //vector<vector<int>> jobs = { {0, 3}, {3, 2}, {2, 7} }; // 15
+
+    cout << solution(jobs);
+
+    return 0;
+}
+
+
