7fbedc85069da5b071e3e7a3f9a642bf5eacd7c7
unknown <insooneelife@naver.com>
2021-06-21 02:40:41 +0900

    euler circuit, trail in undirected graph, directed graph

--- Algorithm/Sources/Graph/6-Euler-Circuit.cpp
+++ Algorithm/Sources/Graph/6-Euler-Circuit.cpp
@@ -1,10 +1,45 @@
 ﻿
+// 오일러 서킷, 오일러 트레일 (무향그래프)
+
+
 #include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 #include "Print.h"
 using namespace std;
+
+void MakeDegree(
+    const vector<vector<int>>& graph,
+    vector<int>& degree)
+{
+    const int N = graph.size();
+
+    degree.clear();
+    degree.assign(N, 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            degree[i] += graph[i][j];
+        }
+    }
+}
+
+// 무향 그래프에서 euler circuit이 성립하려면 모든 차수는 짝수개여야 한다.
+bool CheckEulerCircuit(const vector<int>& degree)
+{
+    for (int i = 0; i < degree.size(); ++i)
+    {
+        if (degree[i] % 2 != 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 
 // undirected graph euler circuit
@@ -26,47 +61,44 @@ void EulerCircuit(
     euler.push_back(node);
 }
 
-// directed graph euler circuit
-void DirectGraphEulerCircuit(
-    vector<vector<int>>& graph,
-    vector<int>& euler,
-    int node)
-{
-    for (int i = 0; i < graph[node].size(); ++i)
-    {
-        while (graph[node][i] > 0)
-        {
-            graph[node][i]--;
-            DirectGraphEulerCircuit(graph, euler, i);
-        }
-    }
-
-    euler.push_back(node);
-}
 
 // interface
 vector<int> EulerCircuit(
     vector<vector<int>>& graph,
-    int node, 
-    bool directed_graph)
+    int node)
 {
     vector<int> euler;
-    if (directed_graph)
-    {
-        DirectGraphEulerCircuit(graph, euler, node);
-    }
-    else
-    {
-        EulerCircuit(graph, euler, node);
-    }
+    EulerCircuit(graph, euler, node);
     reverse(begin(euler), end(euler));
-
     return euler;
 }
 
+// 무향 그래프에서 euler trail은 시작 노드와 끝 노드 외에 모든 노드들은 차수가 짝수개여야 한다.
+bool CheckEulerTrail(const vector<vector<int>>& graph, const vector<int>& degree, vector<int>& start_finish)
+{
+    for (int i = 0; i < degree.size(); ++i)
+    {
+        if (degree[i] % 2 != 0)
+        {
+            start_finish.push_back(i);
+        }
+    }
 
+    return start_finish.size() == 2;
+}
 
-int main()
+// euler trail
+// 차수 확인 후 시작점, 끝점만 정확하게 찾았다면,
+// 두 점 중 하나로 euler circuit을 수행하면 euler trail이 만들어진다.
+vector<int> EulerTrail(vector<vector<int>>& graph, int start_node)
+{
+    vector<int> euler;
+    EulerCircuit(graph, euler, start_node);
+    reverse(begin(euler), end(euler));
+    return euler;
+}
+
+void EulerCircuitExample()
 {
     const int N = 6;
     vector<vector<int>> multigraph =
@@ -79,8 +111,49 @@ int main()
         { 0, 0, 0, 1, 1, 0 }
     };
 
-    vector<int> euler = EulerCircuit(multigraph, 0, false);
+    vector<int> degree;
+    vector<int> euler;
+    MakeDegree(multigraph, degree);
+    Print::Container(degree);
+
+    if (CheckEulerCircuit(degree))
+    {
+        euler = EulerCircuit(multigraph, 0);
+    }
+
     Print::Container(euler);
+}
+
+void EulerTrailExample()
+{
+    const int N = 5;
+    vector<vector<int>> multigraph =
+    {
+        { 2, 1, 1, 0, 0 },
+        { 1, 0, 1, 1, 1 },
+        { 1, 1, 0, 1, 1 },
+        { 0, 1, 1, 0, 1 },
+        { 0, 1, 1, 1, 0 }
+    };
+
+    vector<int> euler;
+    vector<int> degree;
+    vector<int> start_finish;
+    MakeDegree(multigraph, degree);
+    Print::Container(degree);
+
+    if (CheckEulerTrail(multigraph, degree, start_finish))
+    {
+        euler = EulerTrail(multigraph, start_finish[0]);
+    }
+
+    Print::Container(euler);
+}
+
+int main()
+{
+    EulerCircuitExample();
+    EulerTrailExample();
 
     return 0;
 }
---
+++ Algorithm/Sources/Graph/7-Directed-Euler-Circuit.cpp
@@ -0,0 +1,193 @@
+﻿
+// 오일러 서킷, 오일러 트레일 (방향그래프)
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "Print.h"
+using namespace std;
+
+void MakeDegree(
+    const vector<vector<int>>& graph,
+    vector<int>& indegree,
+    vector<int>& outdegree)
+{
+    const int N = graph.size();
+
+    indegree.clear();
+    indegree.assign(N, 0);
+    outdegree.clear();
+    outdegree.assign(N, 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            outdegree[i] += graph[i][j];
+            indegree[i] += graph[j][i];
+        }
+    }
+}
+
+// 방향 그래프에서 euler circuit이 성립하려면 모든 노드에 대해 indegree와 outdegree가 같아야한다.
+bool CheckEulerCircuit(const vector<int>& indegree, const vector<int>& outdegree)
+{
+    return indegree == outdegree;
+}
+
+// directed graph euler circuit
+void DirectGraphEulerCircuit(
+    vector<vector<int>>& graph,
+    vector<int>& euler,
+    int node)
+{
+    for (int i = 0; i < graph[node].size(); ++i)
+    {
+        while (graph[node][i] > 0)
+        {
+            graph[node][i]--;
+            DirectGraphEulerCircuit(graph, euler, i);
+        }
+    }
+
+    euler.push_back(node);
+}
+
+// interface
+vector<int> DirectGraphEulerCircuit(
+    vector<vector<int>>& graph,
+    int node)
+{
+    vector<int> euler;
+    DirectGraphEulerCircuit(graph, euler, node);
+    reverse(begin(euler), end(euler));
+
+    return euler;
+}
+
+// 이 그래프가 오일러 트레일을 갖는 그래프인지 판단하기 위해서,
+// 차수계산 후, 시작 노드, 끝 노드의 차수를 확인해본다.
+// 시작노드 : 노드의 나가는 차수가 들어오는 차수보다 1 크다.
+// 끝노드 : 노드의 나가는 차수가 들어오는 차수보다 1 작다.
+// 그 외 노드 : 나가는 차수와 들어오는 차수는 같다.
+
+bool CheckEulerTrail(
+    const vector<vector<int>>& graph,
+    const vector<int>& indegree,
+    const vector<int>& outdegree,
+    int& start_node,
+    int& finish_node)
+{
+    int startcnt = 0;
+    int finishcnt = 0;
+
+    // 시작, 끝 노드는 유일해야함
+    for (int i = 0; i < indegree.size(); ++i)
+    {
+        if (outdegree[i] == indegree[i] + 1)
+        {
+            start_node = i;
+            startcnt++;
+        }
+        if (outdegree[i] + 1 == indegree[i])
+        {
+            finish_node = i;
+            finishcnt++;
+        }
+    }
+    if (!(startcnt == 1 && finishcnt == 1))
+        return false;
+
+    // 그 외에는 같아야함
+    for (int i = 0; i < indegree.size(); ++i)
+    {
+        if (i == start_node || i == finish_node)
+            continue;
+
+        if (outdegree[i] != indegree[i])
+            return false;
+    }
+
+    return true;
+}
+
+
+// euler trail
+vector<int> DirectGraphEulerTrail(vector<vector<int>>& graph, int start_node)
+{
+    vector<int> euler;
+    DirectGraphEulerCircuit(graph, euler, start_node);
+    reverse(begin(euler), end(euler));
+    return euler;
+}
+
+
+void DirectGraphEulerCircuitExample()
+{
+    const int N = 6;
+    vector<vector<int>> multigraph =
+    {
+        { 0, 0, 1, 0, 0, 0 },
+        { 1, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 1, 0 },
+        { 0, 1, 1, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 1 },
+        { 0, 0, 0, 1, 0, 0 }
+    };
+
+    vector<int> euler;
+    vector<int> indegree;
+    vector<int> outdegree;
+
+    MakeDegree(multigraph, indegree, outdegree);
+    Print::Container(indegree);
+    Print::Container(outdegree);
+
+    if (CheckEulerCircuit(indegree, outdegree))
+    {
+        euler = DirectGraphEulerCircuit(multigraph, 0);
+    }
+    Print::Container(euler);
+}
+
+
+// 방향그래프의 오일러 트레일을 찾기 위해서 시작점을 알아야한다.
+// 오일러 트레일의 시작점은 나가는 차수가 들어오는 차수보다 하나 많은 노드이다.
+void DirectGraphEulerTrailExample()
+{
+    const int N = 5;
+    vector<vector<int>> multigraph =
+    {
+        { 0, 0, 1, 0, 0 },
+        { 1, 0, 0, 0, 1 },
+        { 0, 1, 0, 0, 1 },
+        { 0, 1, 1, 0, 0 },
+        { 0, 0, 0, 1, 0 }
+    };
+
+    vector<int> euler;
+    vector<int> indegree;
+    vector<int> outdegree;
+    int start;
+    int finish;
+    MakeDegree(multigraph, indegree, outdegree);
+    Print::Container(indegree);
+    Print::Container(outdegree);
+
+    if (CheckEulerTrail(multigraph, indegree, outdegree, start, finish))
+    {
+        euler = DirectGraphEulerTrail(multigraph, start);
+    }
+
+    Print::Container(euler);
+}
+
+
+
+int main()
+{
+    DirectGraphEulerCircuitExample();
+    DirectGraphEulerTrailExample();
+    return 0;
+}
