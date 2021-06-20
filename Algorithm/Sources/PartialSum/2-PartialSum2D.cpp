e9ccfeb1e0644c5fe9d1c2a5ac5bb903e92f5a7c
unknown <insooneelife@naver.com>
2021-06-20 16:51:07 +0900

    fixed

--- Algorithm/Sources/PartialSum/2-PartialSum2D.cpp
+++ Algorithm/Sources/PartialSum/2-PartialSum2D.cpp
@@ -21,8 +21,10 @@ long long psum[4][4] =
     0, 0, 0, 0
 };
 
+// 2차원 배열의 부분합
 void PartialSum(int n)
 {
+    // 각 행에 대한 부분합을 먼저 구한다.
     for (int i = 0; i < n; ++i)
     {
         for (int j = 1; j < n; ++j)
@@ -31,6 +33,7 @@ void PartialSum(int n)
         }
     }
 
+    // 행 단위로 부분합을 누적시킨다.
     for (int j = 0; j < n; ++j)
     {
         for (int i = 1; i < n; ++i)
@@ -40,6 +43,9 @@ void PartialSum(int n)
     }
 }
 
+// 행 : y, 열 : x
+// y1, x1 부터 y2, x2 까지의 직사각형 구간의 합
+// 0으로 시작하는 인덱스는 예외처리
 long long GridSum(int y1, int x1, int y2, int x2)
 {
     long long ret = psum[y2][x2];
