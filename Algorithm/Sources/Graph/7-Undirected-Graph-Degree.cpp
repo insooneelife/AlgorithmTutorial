dd01466c4bfcf3e316b4c9fc346de46f2243df0f
unknown <insooneelife@naver.com>
2021-06-26 12:29:38 +0900

    Graph degree updated

---
+++ Algorithm/Sources/Graph/7-Undirected-Graph-Degree.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <vector>
+#include <iostream>
+#include <queue>
+#include "Print.h"
+using namespace std;
+
+void MakeDegree(
+    const vector<vector<int>>& graph,
+    vector<int>& degree)
+{
+    const int N = graph.size();
+
+    degree.clear();
+    degree.assign(N, 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            degree[i] += graph[i][j];
+        }
+    }
+}
+
+
+int main()
+{
+    const int N = 5;
+    vector<vector<int>> multigraph =
+    {
+        { 2, 1, 1, 0, 0 },
+        { 1, 0, 1, 1, 1 },
+        { 1, 1, 0, 1, 1 },
+        { 0, 1, 1, 0, 1 },
+        { 0, 1, 1, 1, 0 }
+    };
+
+    vector<int> degree;
+    MakeDegree(multigraph, degree);
+
+    Print::Container(degree);
+
+    return 0;
+}
+
+
---
+++ Algorithm/Sources/Graph/8-Directed-Graph-Degree.cpp
@@ -0,0 +1,55 @@
+﻿
+#include <vector>
+#include <iostream>
+#include <queue>
+#include "Print.h"
+using namespace std;
+
+void MakeDegree(
+    const vector<vector<int>>& graph,
+    vector<int>& indegree,
+    vector<int>& outdegree)
+{
+    const int N = graph.size();
+
+    indegree.clear();
+    indegree.assign(N, 0);
+    outdegree.clear();
+    outdegree.assign(N, 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            outdegree[i] += graph[i][j];
+            indegree[i] += graph[j][i];
+        }
+    }
+}
+
+
+int main()
+{
+    const int N = 6;
+    vector<vector<int>> multigraph =
+    {
+        { 0, 0, 1, 0, 0, 0 },
+        { 1, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 1, 0 },
+        { 0, 1, 1, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 1 },
+        { 0, 0, 0, 1, 0, 0 }
+    };
+
+    vector<int> indegree, outdegree;
+    MakeDegree(multigraph, indegree, outdegree);
+
+    Print::Container(indegree);
+    Print::Container(outdegree);
+
+    return 0;
+}
+
+
+
+
