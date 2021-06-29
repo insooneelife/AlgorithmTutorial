d77424f2db6986753f80ab9eba9f9a7682cf4180
unknown <insooneelife@naver.com>
2021-06-29 18:23:01 +0900

    dijkstra updated

--- Algorithm/Sources/ShortestPath/1-Dijkstra.cpp
+++ Algorithm/Sources/ShortestPath/1-Dijkstra.cpp
@@ -1,7 +1,11 @@
-﻿// Shortest Path
+﻿// 다익스트라 (Dijkstra)
 
-
-
+// 다익스트라의 경우 기본 골격은 BFS와 비슷하지만,
+// priority queue를 이용한다는 점과,
+// 늦게 삽입된 경로가 더 최단일 수도 있기 때문에 그 경우 최단거리를 갱신해주어야 한다는 점이 다르다.
+// 또한 갱신을 허용해야하기 때문에 따로 정점 방문 배열을 이용하지 않는데,
+// 그로 인해 늦게 삽입된 경로가 기존의 최단경로보다 더 크더라도 덮어씌우는 문제가 발생할 수 있다.
+// 그러므로 그 경우에 대한 예외처리가 필요하다.
 
 #include <queue>
 #include <iostream>
@@ -24,19 +28,21 @@ struct Dist
     }
 };
 
-vector<vector<Edge>> graph;
 
-// 다익스트라의 경우 기본 골격은 BFS와 비슷하지만,
-// priority queue를 이용한다는 점과,
-// 늦게 삽입된 경로가 더 최단일 수도 있기 때문에 그 경우 최단거리를 갱신해주어야 한다는 점이 다르다.
-// 또한 갱신을 허용해야하기 때문에 따로 정점 방문 배열을 이용하지 않는데,
-// 그로 인해 늦게 삽입된 경로가 기존의 최단경로보다 더 크더라도 덮어씌우는 문제가 발생할 수 있다.
-// 그러므로 그 경우에 대한 예외처리가 필요하다.
+// 다익스트라 알고리즘
+// time complexity     O(E * logV)
+// input               방향 그래프, 시작 정점
+// output              모든 정점으로의 최단거리
 vector<int> Dijkstra(const vector<vector<Edge>>& graph, int start)
 {
+    // 각 정점까지의 최단거리를 저장할 배열
+    // 초기값은 무한대로 세팅한다.
     vector<int> distances(graph.size(), numeric_limits<int>::max());
 
+    // (정점, 거리) 쌍을 저장하는 우선순위큐
+    // 거리가 작은 순으로 꺼낸다.
     priority_queue<Dist> pq;
+
     distances[start] = 0;
     pq.push({ start, 0 });
 
@@ -50,6 +56,7 @@ vector<int> Dijkstra(const vector<vector<Edge>>& graph, int start)
         if (distances[v] < dist)
             continue;
 
+        // 모든 간선에 대해,
         for (int i = 0; i < graph[v].size(); ++i)
         {
             int next = graph[v][i].v;
@@ -69,8 +76,17 @@ vector<int> Dijkstra(const vector<vector<Edge>>& graph, int start)
 
 int main()
 {
-    vector<int> dist = Dijkstra(graph, start);
+    vector<vector<Edge>> graph =
+    {
+        {{1, 4}, {2, 1}},
+        {{3, 1}},
+        {{1, 2}, {3, 5}},
+        {{4, 3}},
+        {}
+    };
 
-    Print::Con
+    vector<int> dist = Dijkstra(graph, 0);
+
+    Print::Container(dist);
 }
 
