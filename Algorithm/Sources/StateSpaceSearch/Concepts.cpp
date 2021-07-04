957acb176b361dce7d4b068fd0181a3a030e1274
unknown <insooneelife@naver.com>
2021-07-04 17:22:58 +0900

    updated

--- Algorithm/Sources/StateSpaceSearch/Concepts.cpp
+++ Algorithm/Sources/StateSpaceSearch/Concepts.cpp
@@ -4,19 +4,6 @@
 
 
 
-
-// 암호 만들기 (백트래킹)
-// https://www.acmicpc.net/problem/1759
-
-
-// 알파벳 (백트래킹)
-// https://www.acmicpc.net/problem/1987
-
-
-// 스도쿠 (백트래킹)
-// https://www.acmicpc.net/problem/2580
-
-
 // #풀어보기
 // 달이 차오른다, 가자. (BFS and bitmask)
 // https://www.acmicpc.net/problem/1194
