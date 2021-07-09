2b64a2124b1e0405b17e10918cdd76f067c4130c
unknown <insooneelife@naver.com>
2021-07-10 05:00:29 +0900

    undirected graph

--- Algorithm/Sources/ClassificationOfEdges/3-Classification-Of-Edges-Undirected-Graph.cpp
+++ Algorithm/Sources/ClassificationOfEdges/3-Classification-Of-Edges-Undirected-Graph.cpp
@@ -1,5 +1,6 @@
 ﻿
-// 위상정렬
+// 그래프 간선의 분류
+// 양방향 그래프
 
 #include <iostream>
 #include <vector>
@@ -7,61 +8,49 @@
 #include "Print.h"
 using namespace std;
 
-// 인접리스트 그래프
+// 양방향 인접리스트 그래프
 vector<vector<int>> graph =
 {
-    {3},        // A 0
-    {3},        // B 1
-    {0, 1},     // C 2
-    {6, 7},     // D 3
-    {0, 3, 5},  // E 4
-    {10, 9},    // F 5
-    {8},        // G 6
-    {8, 9},     // H 7
-    {11},       // I 8
-    {11, 12},   // J 9
-    {9},        // K 10
-    {},         // L 11
-    {}          // M 12
+    {1, 2, 7},      // 0
+    {0, 3, 5},      // 1
+    {0, 4},         // 2
+    {1, 4, 5},      // 3
+    {2, 3, 6, 7},   // 4
+    {3, 1},         // 5
+    {4},            // 6
+    {0, 4}          // 7
 };
- 
-vector<bool> visited(graph.size(), false);
-vector<int> order;
 
-void DFS(int node)
+vector<int> discovered(graph.size(), -1);
+vector<bool> finished(graph.size(), false);
+int counter = 0;
+
+// 양방향 간선은 부모를 역방향 간선으로 바로 참조할수도 있는데 이를 방지하기 위해 parent도 저장하여 예외처리한다.
+void DFS(int parent, int node)
 {
-    visited[node] = true;
+    discovered[node] = counter++;
 
     for (int i = 0; i < graph[node].size(); ++i)
     {
         int next = graph[node][i];
-        if (!visited[next])
-            DFS(next);
+
+        // 방문한 적 없다면
+        if (discovered[next] == -1)
+        {
+            cout << node << " -> " << next << " : tree edge" << endl;
+            DFS(node, next);
+        }
+        else if (parent != next && discovered[node] != discovered[next])
+        {
+            cout << node << " -> " << next << " : backward or forward edge" << endl;
+        }
     }
-
-    order.push_back(node);
-}
-
-void TopologicalSort()
-{
-    for (int i = 0; i < graph.size(); ++i)
-    {
-        if (!visited[i])
-            DFS(i);
-    }
-
-    reverse(begin(order), end(order));
 }
 
 int main()
 {
-    
-    TopologicalSort();
 
-    for (int i = 0; i < order.size(); ++i)
-    {
-        cout << (char)(order[i] + 'A') << " ";
-    }
+    DFS(-1, 0);
 
     return 0;
 }
