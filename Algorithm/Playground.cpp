055eb0373520955a1d79644798b4169215cef958
insooneelife <insooneelife@naver.com>
2024-08-24 13:15:09 +0900

    updated array

--- Algorithm/Playground.cpp
+++ Algorithm/Playground.cpp
@@ -8,71 +8,74 @@ class Solution
 {
 public:
 
-	Solution(): cache(101, std::vector<std::vector<int>>(101, std::vector<int>(2))) {}
-
 	int stoneGameII(vector<int>& piles) 
 	{
-		return aliceMaximum(piles, 0, 1, true);
+		int size = piles.size();
+
+		for(int n = size - 1; n >= 0; --n)
+		{
+			for(int m = size; m >= 1; --m)
+			{
+				for(int t = 0; t < 2; ++t)
+				{
+					bool aliceTurn = (t == 1);
+
+					int minmaxVal = 0;
+					if(!aliceTurn)
+					{
+						minmaxVal = std::numeric_limits<int>::max();
+					}
+
+					for(int x = 1;  x <= 2 * m; ++x)
+					{
+						if (n + x > piles.size())
+						{
+							continue;
+						}
+
+						int val = dp[n + x][std::max(m, x)][!aliceTurn];
+
+						if(aliceTurn)
+						{
+							int sum = val;
+							for(int i = n; i < n + x; ++i)
+							{
+								sum += piles[i];
+							}
+
+							minmaxVal = std::max(minmaxVal, sum);
+						}
+						else
+						{
+							minmaxVal = std::min(minmaxVal, val);
+						}
+					}
+
+					dp[n][m][aliceTurn] = minmaxVal;
+				}
+			}
+		}
+
+		//Print(piles.size());
+
+		return dp[0][1][true];
 	}
 
-	int aliceMaximum(const vector<int>& piles, int n, int m, bool aliceTurn)
+
+	void Print(int n) const
 	{
-		if (n == piles.size())
+		for(int i = 0; i < n; ++i)
 		{
-			return 0;
-		}		
-
-		if(cache[n][m][aliceTurn] > 0)
-		{
-			return cache[n][m][aliceTurn];
-		}
-
-		int minmaxVal = 0;
-		if (!aliceTurn)
-		{
-			minmaxVal = std::numeric_limits<int>::max();
-		}
-
-		for (int x = 1; x <= 2 * m; ++x)
-		{
-			if(n + x > piles.size())
+			for (int j = 1; j < n; ++j)
 			{
-				continue;
+				std::cout <<"(" << dp[i][j][0] << " " << dp[i][j][1] << ") ";
 			}
-
-			int prevval = aliceMaximum(piles, n + x, std::max(m, x), !aliceTurn);
-
-			// max
-			if(aliceTurn)
-			{
-				int sum = 0;
-				for (int i = n; i < n + x; ++i)
-				{
-					sum += piles[i];
-				}
-
-				int val = sum + prevval;
-				if(minmaxVal < val)
-				{
-					minmaxVal = val;
-				}
-			}
-			// min
-			else
-			{
-				int val = prevval;
-				if (minmaxVal > val)
-				{
-					minmaxVal = val;
-				}
-			}
+			std::cout << std::endl;
 		}
-		
-		return cache[n][m][aliceTurn] = minmaxVal;
 	}
 
 private:
-	std::vector<std::vector<std::vector<int>>> cache;
+	int dp[101][101][2] = {};
 };
 
 
@@ -80,8 +83,8 @@ int main()
 {
 	//vector<int> piles = { 2,7,9,4,4 };
 
-	vector<int> piles = {1, 2, 3, 4, 5, 100};
-	//vector<int> piles = { 1, 1, 1 };
+	//vector<int> piles = {1, 2, 3, 4, 5, 100};
+	vector<int> piles = { 1, 1, 1 };
 
 	Solution s;
 	cout << s.stoneGameII(piles);
--- Algorithm/Sources/DP/Solutions/LeetCode/1140. Stone Game II.cpp
+++ Algorithm/Sources/DP/Solutions/LeetCode/1140. Stone Game II.cpp
@@ -88,6 +88,82 @@ private:
 };
 
 
+// 배열을 이용한 풀이
+class Solution2
+{
+public:
+
+	int stoneGameII(vector<int>& piles)
+	{
+		int size = piles.size();
+
+		for (int n = size - 1; n >= 0; --n)
+		{
+			for (int m = size; m >= 1; --m)
+			{
+				for (int t = 0; t < 2; ++t)
+				{
+					bool aliceTurn = (t == 1);
+
+					int minmaxVal = 0;
+					if (!aliceTurn)
+					{
+						minmaxVal = std::numeric_limits<int>::max();
+					}
+
+					for (int x = 1; x <= 2 * m; ++x)
+					{
+						if (n + x > piles.size())
+						{
+							continue;
+						}
+
+						int val = dp[n + x][std::max(m, x)][!aliceTurn];
+
+						if (aliceTurn)
+						{
+							int sum = val;
+							for (int i = n; i < n + x; ++i)
+							{
+								sum += piles[i];
+							}
+
+							minmaxVal = std::max(minmaxVal, sum);
+						}
+						else
+						{
+							minmaxVal = std::min(minmaxVal, val);
+						}
+					}
+
+					dp[n][m][aliceTurn] = minmaxVal;
+				}
+			}
+		}
+
+		//Print(piles.size());
+
+		return dp[0][1][true];
+	}
+
+
+	void Print(int n) const
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 1; j < n; ++j)
+			{
+				std::cout << "(" << dp[i][j][0] << " " << dp[i][j][1] << ") ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+private:
+	int dp[101][101][2] = {};
+};
+
+
 int main()
 {
 	//vector<int> piles = { 2,7,9,4,4 };
